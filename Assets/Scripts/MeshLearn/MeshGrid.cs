9f39877fe5eaaa2151cfc8da4cc99b833ad91a6f
ymy534876665 <534876665@qq.com>
2020-12-29 13:53:26 +0800

    mesh

--- Assets/Scripts/MeshLearn/MeshGrid.cs
+++ Assets/Scripts/MeshLearn/MeshGrid.cs
@@ -7,25 +7,64 @@ public class MeshGrid : MonoBehaviour {
 
 	public int xSize, ySize;
 
-
+	/// <summary>
+	/// 顶点数组
+	/// </summary>
 	public Vector3[] Vertexes;
+
+
+	private MeshFilter _meshFilter;
 	void Awake()
 	{
-		StartCoroutine(DrawVertexes());
-		//DrawVertexes();
+		//StartCoroutine(DrawVertexes());
+		DrawVertexes();
 	}
 
-	IEnumerator DrawVertexes()
+	void DrawVertexes()
 	{
 		Vertexes = new Vector3[(xSize + 1) * (ySize + 1)];
-		for (int i = 0, x = 0; x <= xSize; x++)
+		Vector2[] uv = new Vector2[Vertexes.Length];
+		for (int i = 0, y = 0; y <= ySize; y++)
 		{
-			for (int y = 0; y <= ySize; y++,i++)
+			for (int x = 0; x <= xSize; x++,i++)
 			{
 				Vertexes[i] = new Vector3(x, y);
-				yield return new WaitForSeconds(0.05f);
+				uv[i] = new Vector2((float)x/xSize,(float)y/ySize);
 			}
 		}
+		_meshFilter = GetComponent<MeshFilter>();
+		Mesh mesh = new Mesh();
+		_meshFilter.mesh = mesh;
+		mesh.name = "test mesh";
+		mesh.vertices = Vertexes;
+		mesh.uv = uv;
+		//三角形
+		int[] triangles = new int[xSize*6 * ySize];
+		int t1 = 0;
+		int v1 = 0;
+		for (int y = 0; y < ySize; y++)
+		{
+			for (int x = 0; x < xSize; x++)
+			{
+				triangles[t1] = v1;
+				triangles[t1 + 1] = triangles[t1 + 4] = xSize + v1 + 1;
+				triangles[t1 + 2] = triangles[t1 + 3] = v1 + 1;
+				triangles[t1 + 5] = xSize + v1 + 2;
+				t1 += 6;
+				v1++;
+				
+			}
+			v1++;
+		}
+		mesh.triangles = triangles;
+		mesh.RecalculateNormals();
+		//int[] triangles = new int[6];
+		//triangles[0] = 0;
+		//triangles[1] = triangles[4] = xSize + 1;
+		//triangles[2] = triangles[3] = 1;
+		//triangles[5] = xSize + 2;
+
+
 	}
 	void OnDrawGizmos()
 	{
